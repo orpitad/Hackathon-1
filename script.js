5686e6e7d4e40367d1a267e4c41eb18aa20ffb3c
orpitad <95119856+orpitad@users.noreply.github.com>
2022-03-06 08:09:08 +0530

    Handling Errors

--- script.js
+++ script.js
@@ -1,23 +1,34 @@
 var value = "";
 
 function getValue() {
-    const inputElement = document.getElementById("inputName");
-    value = inputElement.value;
-  }
-  
+  const inputElement = document.getElementById("inputName");
+  value = inputElement.value;
+}
+
 function submitName() {
   document.getElementById("result").innerHTML = "";
+
   if (value.includes(",")) {
     let dataResponse = getData(true);
-    dataResponse.then((data) => {
-        console.log(data);
-        data.forEach(item => generateHTML(item))
-      });
-  } else {
-    let dataResponse = getData(false);
+
     dataResponse.then((data) => {
       console.log(data);
-      generateHTML(data);
+      if (data.statusResponse) {
+        showError(data);
+      } else {
+        data.forEach((item) => generateHTML(item));
+      }
+    });
+  } else {
+    let dataResponse = getData(false);
+
+    dataResponse.then((data) => {
+      console.log(data);
+      if (data.statusResponse) {
+        showError(data);
+      } else {
+        generateHTML(data);
+      }
     });
   }
 }
@@ -37,16 +48,28 @@ async function getData(isMultipledata) {
     console.log(response);
     if (response.status == 200) {
       return response.json();
+    } else {
+      return {
+        statusResponse: response.status,
+        statusText: response.statusText,
+      };
     }
-    throw new Error(response.status);
+    // throw new Error(response.status);
   } else {
     const url = "https://api.nationalize.io/?name=" + value.toString();
-    let response = await fetch(url);
+    let response = await fetch(url).catch(function () {
+      console.log("404 Not Found");
+    });
     console.log(response);
     if (response.status == 200) {
       return response.json();
+    } else {
+      return {
+        statusResponse: response.status,
+        statusText: response.statusText,
+      };
     }
-    throw new Error(response.status);
+    // throw new Error(response.status);
   }
 }
 
@@ -58,16 +81,16 @@ function generateHTML(nationalityInfo) {
   let html_div = "";
   if (nationalityInfo.country.length > 0) {
     nationality_name = nationalityInfo.name;
-//     nationalityInfo.country.sort((a, b) =>
-//       a.country_id < b.country_id ? 1 : b.country_id < a.country_id ? -1 : 0
-//     );
+    //     nationalityInfo.country.sort((a, b) =>
+    //       a.country_id < b.country_id ? 1 : b.country_id < a.country_id ? -1 : 0
+    //     );
     console.log(nationalityInfo);
 
     for (let i = 0; i <= 1; i++) {
       if (nationalityInfo.country[i]) {
         nationality_country = nationalityInfo.country[i].country_id;
         nationality_probability = nationalityInfo.country[i].probability;
-        html_div = `<div class="box red">
+        html_div = `<div class="box blue">
                                 <h2><mark>${nationality_name}</mark></h2>
                                 <p><b>Country</b> : ${nationality_country}</p>
                                 <p><b>Probability</b> : ${nationality_probability}</p>
@@ -76,11 +99,18 @@ function generateHTML(nationalityInfo) {
       }
     }
   } else {
-    html_div = `<div class="box red">
+    html_div = `<div class="box blue">
                                 <h2>No Results found...!!</h2>
                             </div> `;
     document.getElementById("result").innerHTML += html_div;
   }
 }
 
-
+function showError(responseError) {
+  let html_div = `<div class="box red">
+        <h2><b>Error</b></h2>
+        <p>Status : ${responseError.statusResponse}</p>
+        <p>Status Text : ${responseError.statusText}</p>
+    </div> `;
+  document.getElementById("result").innerHTML += html_div;
+}
